a530d6ffc55049db2f2697952a0ed3b9dd520376
Learner <linhdqtg@gmail.com>
2019-08-27 04:24:45 +0700

    Remove Copyright

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -2,9 +2,6 @@
 // Copyright (c) 2014-2018, The Monero Project
 // Copyright (c) 2018-2019, The TurtleCoin Developers
 // Copyright (c) 2018-2019, The DeroGold Association
-// Copyright (c) 2019-2019, The TurtleGold Developers
-// Copyright (c) 2019-2019, The BitcoinMono Developers
-// Copyright (c) 2018-2019, The Aeon Classic Developers
 // Copyright (c) 2018-2019, The WrkzCoin developers
 //
 // Please see the included LICENSE file for more information.
